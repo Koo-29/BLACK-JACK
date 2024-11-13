bf148da8a2aa718902828ee84354792c0d261f76
FreaKoo1 <weigangkoo@gmail.com>
2024-11-13 18:45:48 +0800

    test run

--- GameStart.cpp
+++ GameStart.cpp
@@ -3,17 +3,18 @@
 #include "deck.h"
 using namespace std;
 
-int card_1,card_2,card_3,card_4,player_total,dealer_total;
+char card_1,card_2,card_3,card_4;
+int player_total,dealer_total;
 char choice;
+
+
 void playerTurn(){
   card_1 = getCard();
   card_2 = getCard();
-  cout << "Your's hand cards: " << endl;
-  cout << card_1 << " " << card_2 << endl;
+  cout << "Your's hand cards: " << card_1 << " " << card_2  << endl;
   player_total = charToInt(card_1) + charToInt(card_2);
   cout << "Your hand total value is " << player_total << endl;
-  cout << "Do you want to Hit(H) or Sit(S)? ";
-  cin.get(choice);
+
 }
 
 void checkWinner(int player_total,int dealer_total){
@@ -21,44 +22,46 @@ void checkWinner(int player_total,int dealer_total){
     cout << "It's a tie!";
   }
   
-  if (player_total > dealer_total && player_total < 21){
+  else if(player_total > dealer_total && player_total < 21){
     cout << "You wins!";
   }
 
-  if (player_total  < dealer_total && dealer_total < 21 ){
+  else if (player_total  < dealer_total && dealer_total < 21 ){
     cout << "Dealer wins!";
   }
 
-  if (player_total > 21){
+  else if(player_total > 21 && dealer_total < 21){
     cout << "You are bust!";
   }
-  if (dealer_total > 21){
-    cout << "You wins, dealer bust!";
+  else{
+    cout << "Dealer bust!You wins!";
   }
+  
+
 }
 
 void dealerTurn(){
   card_3 = getCard();
   card_4 = getCard();
   dealer_total = charToInt(card_3) + charToInt(card_4);
-  cout << "Dealer's card \n";
-  cout << "[Hidden]  " << card_4 << endl;
-  
-  while(dealer_total < 17){
-    char new_card = getCard();
-    cout << "Dealer hits and draws a new card "<< new_card << endl;
-    dealer_total += charToInt(new_card);
-  }
-
-  cout << "Dealer stands with [Hidden] " << card_4 << endl;
-  }
-
-int option(char option){
-   if (option == 'S'){
-      cout << "You sit with " << card_1 << "and" << card_2 << endl;
+  cout << "Dealer's card "<<  "[Hidden]" <<" " << card_4 << endl;
+}
+void dealerOption() {
+    cout << "Dealer reveals hidden card: " << card_3 << " and " << card_4 << endl;
+    while (dealer_total < 17) {
+        char new_card = getCard();
+        cout << "Dealer hits and draws a new card " << new_card << endl;
+        dealer_total += charToInt(new_card);
+    }
+    cout << "Dealer sits with a total value of " << dealer_total << endl;
+    
+}
+int playerOption(char choice){
+   if (choice == 'S'){
+      cout << "You sit with " << card_1 << " and " << card_2 << endl;
       cout << "Your total value is " << player_total <<endl;
     }
-    if (option == 'H'){
+    if (choice == 'H'){
       char new_card = getCard();
       cout << "You draw a new card " << new_card << endl;
       player_total += charToInt(new_card);
@@ -72,7 +75,17 @@ int main(){
   shuffler();
   dealerTurn();
   playerTurn();
+  do{
+    cout << "Do you want to hit(H) or sit(S) ? " ;
+    cin >> choice;
+    playerOption(choice);
+
+    if (choice == 'S' || player_total > 21){
+      break;
+    }
+  } while(true);
+  dealerOption();
   checkWinner(player_total,dealer_total);
 
-
+  
 }
